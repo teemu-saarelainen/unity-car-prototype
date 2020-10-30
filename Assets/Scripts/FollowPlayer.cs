ca42c7c6b254b637732b948a4098fe37c575b1d1
Teemu Saarelainen <teemu.saarelainen@Teemus-MB-Pro.local>
2020-10-30 08:24:20 +0200

    Create w/Code, Car prototype game

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Add a public variable - to be assigned from editor
+    public GameObject player;
+    // Add a public variable for camera offset
+    public Vector3 camera_offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + camera_offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward for some amount
+        //transform.Translate(0, 0, 0.25f);
+        //transform.Translate(Vector3.forward*0.25f);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+
+    }
+}
